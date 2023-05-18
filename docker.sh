caac807a12785510aafe0b4140058aa659f36cdb
Artem <bondarchuk.artem20@gmail.com>
2023-05-18 21:44:54 +0300

    feat: add email service to .sh

--- docker.sh
+++ docker.sh
@@ -6,3 +6,4 @@ docker build -t order:2 -f services/order/Dockerfile services/order
 docker build -t customer-migrations:1 -f services/customer/migrations/Dockerfile services/customer
 docker build -t car-migrations:1 -f services/car/migrations/Dockerfile services/car
 docker build -t order-migrations:1 -f services/order/migrations/Dockerfile services/order
+docker build -t email:5 -f services/email/Dockerfile services/email
--- helm/v1/build.sh
+++ helm/v1/build.sh
@@ -2,4 +2,5 @@ helm dep build charts/common
 helm dep build charts/client
 helm dep build charts/customer
 helm dep build charts/order
+helm dep build charts/email
 helm dep build .
